186c6897c3938b233cfe3f64213f54222521b724
HOME <HOME@DESKTOP-L6UBTV3>
2023-07-03 18:18:49 -0500

    Agregar archivos de proyecto.

---
+++ aQuincheS5a/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aQuincheS5a
+{
+    public class Estudiante
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string apellido{ get; set; }
+        public string edad { get; set; }
+    }
+}
---
+++ aQuincheS5a/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace aQuincheS5a
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.56.1/ws_uisrael/post.php";
+        private HttpClient client = new HttpClient();
+        private ObservableCollection<Estudiante>post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+        }
+        public async void ObtenerDatos()
+        {
+            var contenido = await client.GetStringAsync(Url);
+            List<Estudiante> listaPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+            post = new ObservableCollection<Estudiante>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+        private async void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
